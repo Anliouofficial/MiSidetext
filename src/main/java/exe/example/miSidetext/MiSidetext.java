271ef19b627a62c3a73aa007c29cfe89b25b9ea4
Anliouofficial <mc_sxy@foxmail.com>
2025-11-05 19:25:22 +0800

    feat: 初始化 Minecraft 插件项目基础结构
    添加 plugin.yml 配置文件、主类文件、.gitignore 和 pom.xml

---
+++ src/main/java/exe/example/miSidetext/MiSidetext.java
@@ -0,0 +1,17 @@
+package exe.example.miSidetext;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MiSidetext extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
